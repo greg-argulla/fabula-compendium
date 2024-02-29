b82e72a4b833429182ed0a9e83751bb2abfa7f3b
greg-argulla <greg.argulla@gmail.com>
2024-02-29 18:38:25 +0800

    fix: push to center

--- src/ChatComponent.jsx
+++ src/ChatComponent.jsx
@@ -137,7 +137,7 @@ const ChatComponent = (props) => {
                 url: "/chatgpt",
                 height: 250,
                 width: 320,
-                anchorOrigin: { horizontal: "RIGHT", vertical: "BOTTOM" },
+                anchorOrigin: { horizontal: "CENTER", vertical: "BOTTOM" },
                 disableClickAway: true,
                 marginThreshold: 10,
               });
