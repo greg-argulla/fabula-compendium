1f61a172d889589fce2991d7cf8619f36aa0662a
greg-argulla <greg.argulla@gmail.com>
2024-02-29 18:45:55 +0800

    fix add the GM helper on the upper right

--- src/ChatComponent.jsx
+++ src/ChatComponent.jsx
@@ -137,7 +137,7 @@ const ChatComponent = (props) => {
                 url: "/chatgpt",
                 height: 250,
                 width: 320,
-                anchorOrigin: { horizontal: "CENTER", vertical: "BOTTOM" },
+                anchorOrigin: { horizontal: "RIGHT", vertical: "TOP" },
                 disableClickAway: true,
                 marginThreshold: 10,
               });
